eea7bc04f6fa7b764121c376e5f6f0c43310a1c1
Nuria López <nulomi189@gmail.com>
2025-10-16 12:59:36 +0200

    ecercisis condi

--- 1-Carpeta/A1_Estroctures_Condicionals/src/a1_estroctures_condicionals/ex2corregido.java
+++ 1-Carpeta/A1_Estroctures_Condicionals/src/a1_estroctures_condicionals/ex2corregido.java
@@ -8,29 +8,27 @@ import java.util.Scanner;
  *
  * @author nlo7503
  */
-public class ex2 {
-    public static void main(String[] args) {
+public class ex2corregido {
+        public static void main(String[] args) {
         // Declarar la variable
-        double num1,num2,divisio,suma,resta,multiplicacio;
+        double num1,num2,suma,resta,multiplicacio,divisio;
         Scanner entrada = new Scanner(System.in);
         // Solicitar la edad
-        System.out.println("introdueix el valor del  numero 1 i numero 2");
+        System.out.print("introdueix el valor del  numero1: ");
         num1 = entrada.nextDouble();
+        System.out.print("introdueix el valor del  numero2: ");
         num2 = entrada.nextDouble();
          suma = num1+num2;
+         System.out.printf("%.2f+%.2f=%.2f\n",num1,num2,suma);
          resta = num1-num2;
+         System.out.printf("%.2f-%.2f=%.2f\n",num1,num2,resta);
          multiplicacio = num1*num2;
+         System.out.printf("%.2f*%.2f=%.2f\n",num1,num2, multiplicacio);
         // Condicionales
             if (num2 == 0) {
-            System.out.printf("La suma es: %.2f\n", suma);
-            System.out.printf("La resta es: %.2f\n", resta);
-            System.out.printf("La multiplicacio es: %.2f\n", multiplicacio);
             System.out.println("La divisio per 0 no es possible.");
         } else {
             divisio = num1 / num2;
-            System.out.printf("La suma es: %.2f\n", suma);
-            System.out.printf("La resta es: %.2f\n", resta);
-            System.out.printf("La multiplicacio és: %.2f\n", multiplicacio);
             System.out.printf("La divisio es: %.2f\n", divisio);
             }
     }
